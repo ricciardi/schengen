892ee8581708142bf3eb8ea5728dd8970a33c7aa
Jason Poulos <jvpoulos@gmail.com>
2020-06-13 22:48:52 -0400

    work on weights matrices

--- README.md
+++ README.md
@@ -5,15 +5,36 @@ Prerequsites
 ------
 
 * **R** >= 3.5.0 (tested on 3.6.1)
- * dplyr, readstata13
+ * dplyr, readstata13, glmnet, boot, doParallel
 
+Install the forked MCPanel repo:
+```R
+install.packages("devtools")
+install.packages("latex2exp")
+library(devtools) 
+install_github("jvpoulos/MCPanel")
+```
 
 Run order
 ------
 
-schengen_MCM_data.R
-Schengen_MCM.R
-mc-plot.R
+1. schengen_MCM_data.R
+2. Schengen_MCM.R
+	* MCEst.R
+3. mc-plot.R
 
-mc-placebo.R
-mc-placebo-plot.R
+4. mc-placebo.R
+5. mc-placebo-plot.R
+
+
+Example usage:
+------
+
+```R
+library(MCPanel)
+estimated_obj <- mcnnm_cv(M, mask, W, to_estimate_u = 0, to_estimate_v = 0, num_lam_L = 40)
+                  
+best_lam_L <- estimated_obj$best_lambda
+estimated_mat <- estimated_obj$L
+
+```
--- schengen_MCM_data.R
+++ schengen_MCM_data.R
@@ -2,6 +2,8 @@
 
 library(dplyr)
 library(readstata13)
+library(glmnet)
+library(caret)
 
 data <- read.dta13("../FINAL.dta", generate.factors=T) # includes variables for 2 analyses + covariates 
 
@@ -49,6 +51,40 @@ length(never.treated.lm[!never.treated.lm%in%switch.treated.lm]) == length(never
 data$quarter <-NA
 data$quarter <- rep(1:4, nrow(data)/4)
 data$yearquarter <- as.numeric(paste0(data$year,data$quarter))
+
+# Covariates matrices
+
+covars <- lapply(covariates, function(c){
+  mat <- reshape(data.frame(data[c("REGION","yearquarter",c)]), idvar = "REGION", timevar = "yearquarter", direction = "wide") # N x T
+  colnames(mat) <- sub(paste0(c,"."),"", colnames(mat))
+  rownames(mat) <- mat$REGION
+  mat <- mat[,-1]
+})
+
+# Time-invariant covariates
+# fixing them at some pre-determined reference years (e.g. 2018 for the retrospective analysis, 2005 for the forward looking analysis).
+
+covars.cbw.eastern <- as.matrix(bind_cols(sapply(1:length(covars), function(i){ # all post-treatment years
+  covars[[i]][,which(colnames(covars[[i]])=='20081'):ncol(covars[[i]])][rownames(covars[[i]]) %in% cbw,]
+}))) # N x # predictors
+rownames(covars.cbw.eastern) <- rownames(covars[[1]][rownames(covars[[1]]) %in% cbw,])
+
+covars.cbw.swiss <- as.matrix(bind_cols(sapply(1:length(covars), function(i){ # all post-treatment years
+  covars[[i]][,which(colnames(covars[[i]])=='20072'):ncol(covars[[i]])][rownames(covars[[i]]) %in% cbw,]
+}))) # N x # predictors
+rownames(covars.cbw.swiss) <- rownames(covars[[1]][rownames(covars[[1]]) %in% cbw,])
+
+covars.lm.eastern <- as.matrix(bind_cols(sapply(1:length(covars), function(i){ # all pre-treatment years
+  covars[[i]][,1:which(colnames(covars[[i]])=='20074')][rownames(covars[[i]]) %in% lm,]
+}))) # N x # predictors
+rownames(covars.lm.eastern) <- rownames(covars[[1]][rownames(covars[[1]]) %in% lm,])
+
+covars.lm.swiss <- as.matrix(bind_cols(sapply(1:length(covars), function(i){ # all pre-treatment years
+  covars[[i]][,1:which(colnames(covars[[i]])=='20071')][rownames(covars[[i]]) %in% lm,]
+}))) # N x # predictors
+rownames(covars.lm.swiss) <- rownames(covars[[1]][rownames(covars[[1]]) %in% lm,])
+
+# TODO: Remove duplicated columns
 
 for(o in outcomes){
   print(o)
@@ -106,6 +142,42 @@ for(o in outcomes){
   for(i in switch.treated.lm){
     mask.lm.swiss[,colnames(mask.lm.swiss)%in%data$yearquarter[data$REGION==i & data$FoM_LM==1 & data$treated_LM=="2007: FoM; 2009: Schengen"]][rownames(mask.lm.swiss)%in%c(i),] <- 1 # FoM_X=1 from 2007Q2 and SCHENGEN_X=1 from 2009Q1
   }
+
+  
+  # # estimate propensity scores with time-invariant covariates
+  # 
+  # logitMod.x <- cv.glmnet(x=capacity.covars.x, y=as.factor((1-mask.cbw.eastern)[,"20081"]), family="binomial", nfolds= nrow(capacity.covars.x), parallel = TRUE, nlambda=400) # LOO
+  # 
+  # logitMod.z <- cv.glmnet(x=capacity.covars.z, y=as.factor((1-treat_mat)[treated.indices[1],]), family="binomial", nfolds=nrow(capacity.covars.z), parallel = TRUE, nlambda=400)
+  # 
+  # p.weights.x <- as.vector(predict(logitMod.x, capacity.covars.x, type="response", s ="lambda.min"))
+  # p.weights.z <- as.vector(predict(logitMod.z, capacity.covars.z, type="response", s ="lambda.min"))
+  # 
+  # p.weights <- outer(p.weights.x,p.weights.z)   # outer product of fitted values on response scale
+  # p.weights <- t(p.weights) # T x N
+  # rownames(p.weights) <-rownames(data)
+  # colnames(p.weights) <-colnames(data)
+  # 
+  # train_w <- p.weights[,colnames(p.weights)%in%colnames(train_data)][rownames(p.weights)%in%rownames(train_data),]
+  # test_w <- p.weights[,colnames(p.weights)%in%colnames(test_data)][rownames(p.weights)%in%rownames(test_data),]
+  
+  # weights matrix
+  weights.cbw.eastern <- matrix(1/dim(data.cbw)[1]*dim(data.cbw)[2], nrow = nrow(data.cbw ), # equal unit weights
+                             ncol= ncol(data.cbw),
+                             dimnames = list(rownames(data.cbw), colnames(data.cbw))) # (N x T)
+  
+  weights.cbw.swiss <- matrix(1/dim(data.cbw)[1]*dim(data.cbw)[2], nrow = nrow(data.cbw ), 
+                           ncol= ncol(data.cbw),
+                           dimnames = list(rownames(data.cbw), colnames(data.cbw))) # (N x T)
+  
+  weights.lm.eastern <- matrix(1/dim(data.cbw)[1]*dim(data.cbw)[2], nrow = nrow(data.lm ), 
+                            ncol= ncol(data.lm),
+                            dimnames = list(rownames(data.lm), colnames(data.lm))) # (N x T)
+  
+  weights.lm.swiss <- matrix(1/dim(data.cbw)[1]*dim(data.cbw)[2], nrow = nrow(data.lm ), 
+                          ncol= ncol(data.lm),
+                          dimnames = list(rownames(data.lm), colnames(data.lm))) # (N x T)
+  
   # Save
   
   outcomes.cbw.eastern <- list("M"=data.cbw, "mask"=mask.cbw.eastern,"treated"=switch.treated.cbw,"control"=always.treated.cbw)
@@ -120,15 +192,3 @@ for(o in outcomes){
   saveRDS(outcomes.cbw.swiss, paste0("data/outcomes-cbw-swiss-",o,".rds"))
   saveRDS(outcomes.lm.swiss,paste0("data/outcomes-lm-swiss-",o,".rds"))
 }
-
-# Covariates matrices
-
-covars <- lapply(covariates, function(c){
-  mat <- reshape(data.frame(data[c("REGION","yearquarter",c)]), idvar = "REGION", timevar = "yearquarter", direction = "wide") # N x T
-  colnames(mat) <- sub(paste0(c,"."),"", colnames(mat))
-  rownames(mat) <- mat$REGION
-  mat <- mat[,-1]
-})
-
-names(covars) <- covariates
-saveRDS(covars,"data/covariates.rds")
