3513a77957cb042af9a4dfa890203efb7d91607e
Jason Poulos <jvpoulos@gmail.com>
2020-11-20 10:52:07 -0500

    update

--- mc-plot.R
+++ mc-plot.R
@@ -14,45 +14,68 @@ library(boot)
 
 source("TsPlot.R")
 
-PlotMCCapacity <- function(observed,main,y.title,mc_est,boot_result,treated,control,eastern,swiss,vline,vline2,breaks,labels,att.label,rev){
+PlotMCCapacity <- function(observed,main,y.title,mc_est_eastern,mc_est_swiss,boot_result_eastern,boot_result_swiss,treated,control,eastern,swiss,vline,vline2,breaks,labels,att.label,rev){
   ## Create time series data
   
-  predicted <- mc_est$Mhat
+  predicted.eastern <- mc_est_eastern$Mhat
+  pointwise.eastern <- mc_est_eastern$impact # = boot_result$t0
   
-  pointwise <- mc_est$impact # = boot_result$t0
+  predicted.swiss <- mc_est_swiss$Mhat
+  pointwise.swiss <- mc_est_swiss$impact # = boot_result$t0
   
   # Create confidence intervals
   
-  boot.ci.lower <-matrix(apply(boot_result$t, 2, function(x) quantile(x, 0.025)), nrow=dim(pointwise)[1], ncol=dim(pointwise)[2], byrow=FALSE,
-                              dimnames=dimnames(pointwise)) # alpha/2
-  boot.ci.upper <-matrix(apply(boot_result$t, 2, function(x) quantile(x, 0.975)), nrow=dim(pointwise)[1], ncol=dim(pointwise)[2], byrow=FALSE,
-                              dimnames=dimnames(pointwise)) # 1-alpha/2
+  boot.ci.lower.eastern <-matrix(apply(boot_result_eastern$t, 2, function(x) quantile(x, 0.025)), nrow=dim(pointwise.eastern)[1], ncol=dim(pointwise.eastern)[2], byrow=FALSE,
+                              dimnames=dimnames(pointwise.eastern)) # alpha/2
+  boot.ci.upper.eastern <-matrix(apply(boot_result_eastern$t, 2, function(x) quantile(x, 0.975)), nrow=dim(pointwise.eastern)[1], ncol=dim(pointwise.eastern)[2], byrow=FALSE,
+                              dimnames=dimnames(pointwise.eastern)) # 1-alpha/2
+  
+  boot.ci.lower.swiss <-matrix(apply(boot_result_swiss$t, 2, function(x) quantile(x, 0.025)), nrow=dim(pointwise.swiss)[1], ncol=dim(pointwise.swiss)[2], byrow=FALSE,
+                                 dimnames=dimnames(pointwise.swiss)) # alpha/2
+  boot.ci.upper.swiss <-matrix(apply(boot_result_swiss$t, 2, function(x) quantile(x, 0.975)), nrow=dim(pointwise.swiss)[1], ncol=dim(pointwise.swiss)[2], byrow=FALSE,
+                                 dimnames=dimnames(pointwise.swiss)) # 1-alpha/2
   
   ## Plot time series 
   
-  treat.status <- matrix(rownames(pointwise), nrow=nrow(pointwise), ncol=1)
-  treat.status[rownames(pointwise) %in% eastern] <- "eastern"
-  treat.status[rownames(pointwise) %in% swiss] <- "swiss"
-  treat.status[rownames(pointwise) %in% control] <- "control"
+  treat.status <- matrix(rownames(observed), nrow=nrow(observed), ncol=1)
+  treat.status[rownames(observed) %in% eastern] <- "eastern"
+  treat.status[rownames(observed) %in% swiss] <- "swiss"
+  treat.status[rownames(observed) %in% control] <- "control"
   treat.status <- matrix(treat.status, dimnames=list(NULL, "status"))
   
+  treat.status.eastern <- matrix(rownames(pointwise.eastern), nrow=nrow(pointwise.eastern), ncol=1)
+  treat.status.eastern[rownames(pointwise.eastern) %in% eastern] <- "eastern"
+  treat.status.eastern[rownames(pointwise.eastern) %in% control] <- "control"
+  treat.status.eastern <- matrix(treat.status.eastern, dimnames=list(NULL, "status"))
+  
+  treat.status.swiss <- matrix(rownames(pointwise.swiss), nrow=nrow(pointwise.swiss), ncol=1)
+  treat.status.swiss[rownames(pointwise.swiss) %in% swiss] <- "swiss"
+  treat.status.swiss[rownames(pointwise.swiss) %in% control] <- "control"
+  treat.status.swiss <- matrix(treat.status.swiss, dimnames=list(NULL, "status"))
+  
   observed.mean <-  aggregate(observed, list(treat.status), mean)[-1]
-  predicted.mean <-  aggregate(predicted, list(treat.status), mean)[-1]
-  pointwise.mean <- aggregate(pointwise, list(treat.status), mean)[-1]
-  pointwise.ci.lower.mean <- aggregate(boot.ci.lower, list(treat.status), mean)[-1]
-  pointwise.ci.upper.mean <- aggregate(boot.ci.upper, list(treat.status), mean)[-1]
+  
+  predicted.mean.eastern <-  aggregate(predicted.eastern, list(treat.status.eastern), mean)[-1]
+  pointwise.mean.eastern <- aggregate(pointwise.eastern, list(treat.status.eastern), mean)[-1]
+  pointwise.ci.lower.mean.eastern <- aggregate(boot.ci.lower.eastern, list(treat.status.eastern), mean)[-1]
+  pointwise.ci.upper.mean.eastern <- aggregate(boot.ci.upper.eastern, list(treat.status.eastern), mean)[-1]
+  
+  predicted.mean.swiss <-  aggregate(predicted.swiss, list(treat.status.swiss), mean)[2,][-1]
+  pointwise.mean.swiss <- aggregate(pointwise.swiss, list(treat.status.swiss), mean)[2,][-1]
+  pointwise.ci.lower.mean.swiss <- aggregate(boot.ci.lower.swiss, list(treat.status.swiss), mean)[2,][-1]
+  pointwise.ci.upper.mean.swiss <- aggregate(boot.ci.upper.swiss, list(treat.status.swiss), mean)[2,][-1]
 
-  ts.means <- cbind(t(observed.mean), t(predicted.mean), t(pointwise.mean))
+  ts.means <- cbind(t(observed.mean), t(predicted.mean.eastern), t(predicted.mean.swiss), t(pointwise.mean.eastern), t(pointwise.mean.swiss))
   colnames(ts.means) <- c("observed.control","observed.eastern","observed.swiss","predicted.control","predicted.eastern","predicted.swiss","pointwise.control","pointwise.eastern","pointwise.swiss")
   ts.means <- cbind(ts.means, "year"=as.numeric(rownames(ts.means)))
   ts.means.m <- melt(data.frame(ts.means), id.var=c("year"))
   
-  ts.ci.lower.means <- t(pointwise.ci.lower.mean)
+  ts.ci.lower.means <- cbind(t(pointwise.ci.lower.mean.eastern),t(pointwise.ci.lower.mean.swiss))
   colnames(ts.ci.lower.means) <- c("pointwise.control","pointwise.eastern","pointwise.swiss")
   ts.ci.lower.means <- cbind(ts.ci.lower.means, "year"=as.numeric(rownames(ts.means)))
   ts.ci.lower.means.m <- melt(data.frame(ts.ci.lower.means), id.var=c("year"))
   
-  ts.ci.upper.means <- t(pointwise.ci.upper.mean)
+  ts.ci.upper.means <- cbind(t(pointwise.ci.upper.mean.eastern),t(pointwise.ci.upper.mean.swiss))
   colnames(ts.ci.upper.means) <- c("pointwise.control","pointwise.eastern","pointwise.swiss")
   ts.ci.upper.means <- cbind(ts.ci.upper.means, "year"=as.numeric(rownames(ts.means)))
   ts.ci.upper.means.m <- melt(data.frame(ts.ci.upper.means), id.var=c("year"))
@@ -79,16 +102,16 @@ PlotMCCapacity <- function(observed,main,y.title,mc_est,boot_result,treated,cont
   ts.means.m$hline <-NA
   ts.means.m$hline[ts.means.m$series!="Timeseries"] <-0
   
-  if(rev){
-    ts.means.m$value[ts.means.m$year > vline2 & (ts.means.m$variable=="pointwise.eastern" | ts.means.m$variable=="predicted.eastern")] <- NA # censor 
-    ts.means.m$value[ts.means.m$year > vline & (ts.means.m$variable=="pointwise.swiss" | ts.means.m$variable=="predicted.swiss")] <- NA
-    
-    ts.means.m$upper[ts.means.m$year > vline2 & (ts.means.m$variable=="pointwise.eastern" | ts.means.m$variable=="predicted.eastern")] <- NA 
-    ts.means.m$upper[ts.means.m$year > vline & (ts.means.m$variable=="pointwise.swiss" | ts.means.m$variable=="predicted.swiss")] <- NA
-    
-    ts.means.m$lower[ts.means.m$year > vline2 & ts.means.m$variable=="pointwise.eastern"] <- NA 
-    ts.means.m$lower[ts.means.m$year > vline & ts.means.m$variable=="pointwise.swiss"] <- NA
-  }
+  # if(rev){
+  #   ts.means.m$value[ts.means.m$year > vline2 & (ts.means.m$variable=="pointwise.eastern" | ts.means.m$variable=="predicted.eastern")] <- NA # censor 
+  #   ts.means.m$value[ts.means.m$year > vline & (ts.means.m$variable=="pointwise.swiss" | ts.means.m$variable=="predicted.swiss")] <- NA
+  #   
+  #   ts.means.m$upper[ts.means.m$year > vline2 & (ts.means.m$variable=="pointwise.eastern" | ts.means.m$variable=="predicted.eastern")] <- NA 
+  #   ts.means.m$upper[ts.means.m$year > vline & (ts.means.m$variable=="pointwise.swiss" | ts.means.m$variable=="predicted.swiss")] <- NA
+  #   
+  #   ts.means.m$lower[ts.means.m$year > vline2 & ts.means.m$variable=="pointwise.eastern"] <- NA 
+  #   ts.means.m$lower[ts.means.m$year > vline & ts.means.m$variable=="pointwise.swiss"] <- NA
+  # }
   
   ts.plot <- TsPlot(df=ts.means.m,main=main, y.title=y.title,vline,vline2,breaks,labels,hline=ts.means.m$hline,rev)
   
@@ -116,16 +139,21 @@ for(o in outcome.vars){
     ## Analysis 1: ST vs AT (retrospective, X=CBW) 
     
     outcomes.cbw <- readRDS(paste0("data/outcomes-cbw-",o,".rds"))
-    mc.estimates.cbw <- readRDS(paste0("results/mc-estimates-cbw-",o,cf,".rds"))
     
-    boot.cbw <- readRDS(paste0("results/boot-cbw-",o,cf,".rds"))
+    mc.estimates.cbw.eastern <- readRDS(paste0("results/mc-estimates-cbw-eastern-",o,cf,".rds"))
+    boot.cbw.eastern <- readRDS(paste0("results/boot-cbw-eastern-",o,cf,".rds"))
+    
+    mc.estimates.cbw.swiss <- readRDS(paste0("results/mc-estimates-cbw-swiss-",o,cf,".rds"))
+    boot.cbw.swiss <- readRDS(paste0("results/boot-cbw-swiss-",o,cf,".rds"))
     
     mc.plot <- PlotMCCapacity(observed = outcomes.cbw$M, 
                               y.title=outcomes.labels[which(outcome.vars==o)],
                            #   main = "Retrospective prediction for later-treated, by cluster",
                               main= "",
-                              mc_est=mc.estimates.cbw, 
-                              boot_result=boot.cbw,
+                              mc_est_eastern=mc.estimates.cbw.eastern, 
+                              mc_est_swiss=mc.estimates.cbw.swiss,
+                              boot_result_eastern=boot.cbw.eastern,
+                              boot_result_swiss=boot.cbw.swiss,
                               treated=outcomes.cbw$treated, 
                               control=outcomes.cbw$control, 
                               eastern =outcomes.cbw$eastern,
