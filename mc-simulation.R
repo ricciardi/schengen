ee5648186e4a71bcdb66dff59cf124c12690269f
Jason Poulos <jvpoulos@gmail.com>
2021-10-27 10:50:50 -0400

    fix treat mat

--- mc-simulation.R
+++ mc-simulation.R
@@ -8,17 +8,35 @@ library(boot)
 library(Matrix)
 library(tictoc)
 
-# Setup parallel processing 
-library(parallel)
-library(doParallel)
-library(foreach)
+source('boundProbs.R')
+source('bootTraj.R')
 
-cores <- parallel::detectCores()
-print(paste0("number of cores used: ", cores))
-
-cl <- parallel::makeCluster(cores, outfile="")
-
-doParallel::registerDoParallel(cl) # register cluster
+# Setup parallel processing
+doMPI <- TRUE
+if(doMPI){
+  library(doMPI)
+  
+  # Start cluster
+  cl <- startMPIcluster()
+  
+  # Register cluster
+  registerDoMPI(cl)
+  
+  # Check cluster size
+  print(paste0("cluster size: ", clusterSize(cl)))
+  
+} else{
+  library(parallel)
+  library(doParallel)
+  library(foreach)
+  
+  cores <- parallel::detectCores()
+  print(paste0("number of cores used: ", cores))
+  
+  cl <- parallel::makeCluster(cores, outfile="")
+  
+  doParallel::registerDoParallel(cl) # register cluster
+}
 
 MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
   
@@ -43,8 +61,8 @@ MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
 
   # Assignment mechanism 
   
-  C <- replicate(R,rbinom(N,1,0.5))
-  D <- replicate(T,rbinom(R,1,0.5))
+  C <- replicate(R,rbinom(N,1,0.3))
+  D <- replicate(T,rbinom(R,1,0.3))
   
   e <-1/(1+exp(C%*%D + X%*%replicate(T,as.vector(beta)) + replicate(T,delta) + t(replicate(N,gamma)) + noise)) #incl. noise
   
@@ -61,6 +79,8 @@ MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
     }
   }
   
+  fr_obs <- sum(treat_mat)/(N*T) # store fraction observed entries
+  
   # Get vector of initial treatment periods
   
   T0 <- aggregate(col ~ row,
@@ -70,24 +90,31 @@ MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
   # Observed outcome
 
   mask <- treat_mat 
-  
   obs_mat <- noisy_mat * mask 
+  
+  # CV lambda values
+  
+  lambda_L_endog <- as.numeric(sort(c(0.0201769,0.0202961,0.0258464,0.0239927,0.0202191,0.0216138,0.021132,0.0216067,0.0232151,0.021332,0.0201098,0.0203622,0.0248053,0.0219271,0.0196738,0.0237891)))
+  lambda_L_pweights <- as.numeric(sort(c(0,8.69584e-05,9.64993e-05,0.000886265,8.71857e-05,9.21348e-05,0.000839902,9.90469e-05,0.000888573,0.000879063,9.11552e-05,8.59036e-05,9.29274e-05,9.0942e-05,9.83006e-05)))
+  lambda_B_pweights <- as.numeric(sort(c(0,0.00886265,0.00964993,0.000871857,0.00921348,8.39902e-06,0.000990469,0.00888573,0.000860964,0.000859036,0.00929274,0.0090942,0.00983006)))
+  lambda_L_plain <- as.numeric(sort(c(0,0.00149916,0.00153424,0.0090925,0.00344375,0.000557275,0.000584998,0.00331317,0.00146644,0.00391252,0.00407219,0.00133857,0.0038706,0.00133738,0.000108937,0.00141742)))
+  lambda_L_weights <- as.numeric(sort(c(0.00153424,0.00355506,0.0090925,0.00344375,0.00132151,0.00328972,0.00331313,0.00347747,0.00391252,0.0040722,0.00317424,0.00142967,0.00917863,0.00317143,0.0014527,0.00336123)))
+  lambda_L_covars <- as.numeric(sort(c(0,0.00115052,0.00112421,0.012125,0.00108901,0.0010477,0.00109967,0.000123725,0.00128774,0.00100378, 0.00122399,0.0010029,0.00106291)))
+  lambda_B_covars <- as.numeric(sort(c(0.0115052,0.112421,0.012125,0.108901,0.00990989,0.010403,0.10477,0.109967,0.0123725,0.128774,0.100378,0.010721,0.122399,0.10029,0.0108937,0.106291)))
   
   # Impute endogenous covariate values
   
-  est_model_endogenous <- mcnnm_cv(M = X, mask = mask, W = matrix(1, nrow(mask),ncol(mask)), to_estimate_u = 1, to_estimate_v = 1, 
-                                   num_lam_L = 10, niter = 1000, rel_tol = 1e-05, cv_ratio = 0.8, num_folds = 3, is_quiet = 0) # outcome is covariate, prop. weights are equal
+  est_model_endogenous <- mcnnm(M = X, mask = mask, W = matrix(1, nrow(mask),ncol(mask)), to_estimate_u = 1, to_estimate_v = 1, 
+                                lambda_L = median(lambda_L_endog), niter = 1000, rel_tol = 1e-05, is_quiet = 1)[[1]] # outcome is covariate, prop. weights are equal
   est_model_endogenous$Mhat <- est_model_endogenous$L + replicate(T,est_model_endogenous$u) + t(replicate(N,est_model_endogenous$v))
 
   X.hat <- X*mask + est_model_endogenous$Mhat*(1-mask) # only endogenous values imputed
   
   # Estimate propensity weights by matrix completion
 
-  est_model_pweights <- mcnnm_wc_cv(M = (1-mask), C = X.hat, mask =  matrix(1, nrow(mask),ncol(mask)), # no missing entries
-                                    W = matrix(1, nrow(mask),ncol(mask)), to_normalize = 1, to_estimate_u = 1, to_estimate_v = 1, num_lam_L = 5, num_lam_B = 5, niter = 1000, rel_tol = 1e-05, cv_ratio = 0.8, num_folds = 3, is_quiet = 0) # use X with imputed endogenous values
+  est_model_pweights <- mcnnm_wc(M = (1-mask), C = X.hat, mask =  matrix(1, nrow(mask),ncol(mask)), # no missing entries
+                                    W = matrix(1, nrow(mask),ncol(mask)), to_normalize = 1, to_estimate_u = 1, to_estimate_v = 1, lambda_L = median(lambda_L_pweights), lambda_B = median(lambda_B_pweights), niter = 1000, rel_tol = 1e-05, is_quiet = 1)[[1]] # use X with imputed endogenous values
   est_model_pweights$Mhat <- est_model_pweights$L + X.hat%*%replicate(T,as.vector(est_model_pweights$B)) + replicate(T,est_model_pweights$u) + t(replicate(N,est_model_pweights$v)) # use X with imputed endogenous values
-  
-  source('boundProbs.R')
   
   weights <- (1-boundProbs(est_model_pweights$Mhat))/boundProbs(est_model_pweights$Mhat)
   
@@ -98,21 +125,12 @@ MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
   }
          
   weights <- (1-diag(z_weights)*weights)/(diag(z_weights)*weights) # elapsed-time weighting
-
-  # Function for bootstrap
-  BootTraj <- function(impact,indices,mask) {
-    nzmean <- function(x) {
-      if (all(x==0)) 0 else mean(x[x!=0])
-    }
-    att <- apply(impact*(1-mask),1,nzmean)[indices] 
-    return(mean(att[which(rowSums(mask)<ncol(mask))])) # avg over treated units
-  }
   
   ## ------ ------ ------ ------ ------
   ## MC-NNM plain (no weighting, no covariates)
   ## ------ ------ ------ ------ ------
   
-  est_mc_plain <- mcnnm_cv(M = obs_mat, mask = mask, W = matrix(1, nrow(mask),ncol(mask)), to_estimate_u = 1, to_estimate_v = 1, num_lam_L = 10, niter = 1000, rel_tol = 1e-05, cv_ratio = 0.8, num_folds = 3, is_quiet = 0)
+  est_mc_plain <- mcnnm(M = obs_mat, mask = mask, W = matrix(1, nrow(mask),ncol(mask)), to_estimate_u = 1, to_estimate_v = 1, lambda_L = median(lambda_L_plain), niter = 1000, rel_tol = 1e-05, is_quiet = 1)[[1]]
   est_mc_plain$Mhat <- est_mc_plain$L + replicate(T,est_mc_plain$u) + t(replicate(N,est_mc_plain$v))
   est_mc_plain$impact <- (est_mc_plain$Mhat-noisy_mat) # estimated treatment effect
   est_mc_plain$err <- (est_mc_plain$Mhat - true_mat_1) # error (wrt to ground truth)
@@ -136,7 +154,7 @@ MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
   ## MC-NNM + weights (no covariates)
   ## ------ ------ ------ ------ ------
   
-  est_mc_weights <- mcnnm_cv(M = obs_mat, mask = mask, W = weights, to_estimate_u = 1, to_estimate_v = 1, num_lam_L = 10, niter = 1000, rel_tol = 1e-05, cv_ratio = 0.8, num_folds = 3, is_quiet = 0)
+  est_mc_weights <- mcnnm(M = obs_mat, mask = mask, W = weights, to_estimate_u = 1, to_estimate_v = 1, lambda_L = median(lambda_L_weights), niter = 1000, rel_tol = 1e-05, is_quiet = 1)[[1]]
   est_mc_weights$Mhat <- est_mc_weights$L + replicate(T,est_mc_weights$u) + t(replicate(N,est_mc_weights$v))
   est_mc_weights$impact <- (est_mc_weights$Mhat-noisy_mat) # estimated treatment effect
   est_mc_weights$err <- (est_mc_weights$Mhat - true_mat_1) # error (wrt to ground truth)
@@ -160,14 +178,14 @@ MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
   ## MC-NNM + weights + covariates
   ## ------ ------ ------ ------ ------
   
-  est_mc_weights_covars <- mcnnm_wc_cv(M = obs_mat, C = X, mask = mask, W = weights, to_estimate_u = 1, to_estimate_v = 1, num_lam_L = 5, num_lam_B = 5, niter = 1000, rel_tol = 1e-05, cv_ratio = 0.8, num_folds = 3, is_quiet = 0)
+  est_mc_weights_covars <- mcnnm_wc(M = obs_mat, C = X, mask = mask, W = weights, to_estimate_u = 1, to_estimate_v = 1, lambda_L = median(lambda_L_covars), lambda_B = median(lambda_B_covars), niter = 1000, rel_tol = 1e-05, is_quiet = 1)[[1]]
   est_mc_weights_covars$Mhat <- est_mc_weights_covars$L + X.hat%*%replicate(T,as.vector(est_mc_weights_covars$B)) + replicate(T,est_mc_weights_covars$u) + t(replicate(N,est_mc_weights_covars$v))
   est_mc_weights_covars$impact <- (est_mc_weights_covars$Mhat-noisy_mat) # estimated treatment effect
   est_mc_weights_covars$err <- (est_mc_weights_covars$Mhat - true_mat_1) # error (wrt to ground truth)
 
   est_mc_weights_covars$msk_err <- est_mc_weights_covars$err*(1-mask) # masked error (wrt to ground truth)
   est_mc_weights_covars$test_RMSE <- sqrt((1/sum(1-mask)) * sum(est_mc_weights_covars$msk_err^2)) # RMSE on test set (wrt to ground truth)
-  print(paste("MC-NNM (covariates) RMSE:", round(est_mc_weights_covars$test_RMSE,3)))
+  print(paste("MC-NNM (weights + covariates) RMSE:", round(est_mc_weights_covars$test_RMSE,3)))
   
   est_mc_weights_covars$boot <- boot(est_mc_weights_covars$impact, 
                               BootTraj, 
@@ -179,18 +197,23 @@ MCsim <- function(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n){
   est_mc_weights_covars$boot_ci <- boot.ci(est_mc_weights_covars$boot ,type=c("perc"))$percent[-c(1:3)] 
   est_mc_weights_covars$bias <- est_mc_weights_covars$boot_t0-effect_size
   est_mc_weights_covars$cp <- as.numeric((est_mc_weights_covars$boot_ci[1] < effect_size) & (est_mc_weights_covars$boot_ci[2] > effect_size))
+  
+  # cleanup
+  rm(A,C,e,mask,noise,noisy_mat,obs_mat,treat_mat,true_mat_0,true_mat_1,X,X.hat,weights,z_weights)
+  gc()
+  cat(paste("Done with simulation run number",n, "\n"))
     
-  return(list("N"=N, "T"=T, "R"=R, "noise_sc"=noise_sc,"delta_sc"=delta_sc, "gamma_sc"=gamma_sc,"beta_sc"=beta_sc, "effect_size"=effect_size, 
+  return(list("N"=N, "T"=T, "R"=R, "noise_sc"=noise_sc,"delta_sc"=delta_sc, "gamma_sc"=gamma_sc,"beta_sc"=beta_sc, "effect_size"=effect_size, "fr_obs"= fr_obs, 
               "est_mc_plain_RMSE"=est_mc_plain$test_RMSE,"est_mc_plain_bias"=est_mc_plain$bias,"est_mc_plain_cp"=est_mc_plain$cp,
               "est_mc_weights_RMSE"=est_mc_weights$test_RMSE,"est_mc_weights_bias"=est_mc_weights$bias,"est_mc_weights_cp"=est_mc_weights$cp,
               "est_mc_weights_covars_RMSE"=est_mc_weights_covars$test_RMSE,"est_mc_weights_covars_bias"=est_mc_weights_covars$bias,"est_mc_weights_covars_cp"=est_mc_weights_covars$cp))
 }
 
 # define settings for simulation
-settings <- expand.grid("NT"=c(1600,2500),
-                        "noise_sc"=c(0.2,0.4),
-                        "effect_size"=c(0.005,0.01),
-                        "R" = c(5,15))
+settings <- expand.grid("NT"=c(1600,2500,3600),
+                        "noise_sc"=c(0.1,0.2,0.3),
+                        "effect_size"=c(0.001,0.005,0.01),
+                        "R" = c(10,20,40))
 
 args <- as.numeric(commandArgs(trailingOnly = TRUE)) # command line arguments
 thisrun <- settings[args,] 
@@ -213,8 +236,14 @@ tic(print(paste0("setting: ",setting)))
 results <- foreach(i = 1:n.runs, .combine='cbind', .packages =c("MCPanel","matrixStats","boot","Matrix")) %dopar% {
   MCsim(N,T,R,noise_sc,delta_sc,gamma_sc,beta_sc,effect_size,n=i)
 }
-results <- matrix(unlist(results), ncol = n, byrow = FALSE) # coerce into matrix
+results <- matrix(unlist(results), ncol = n, byrow = FALSE, .inorder=FALSE) # coerce into matrix
 saveRDS(results, paste0("results_","N_",N,"_T_",T,"_R_", R,"_noise_sc_",noise_sc,"_effect_size_",effect_size,"_n_",n,".rds"))
 
-stopCluster(cl)
 print(toc())
+
+if(doMPI){
+  closeCluster(cl) # close down MPIcluster
+  mpi.finalize()
+}else{
+  stopCluster(cl)
+}
